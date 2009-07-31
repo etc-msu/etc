7470c5eb5aa943b81fe69194bd188aa0e2fa7f3e
Benjamin Kreeger <benjaminkreeger@gmail.com>
2009-07-31 13:19:19 -0500

    Added temporary form links to old site files, hosted on ETC0.

--- etc/urls.py
+++ etc/urls.py
@@ -4,6 +4,8 @@ from etc.settings import PROJECT_DIR
 urlpatterns = patterns('',
     url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': PROJECT_DIR + '/media'}),
     url(r'^$', 'django.views.generic.simple.direct_to_template', {'template': 'index.html'}),
-	url(r'^service_request.html$', 'etc.views.service'),
+	url(r'^faculty_request_form.html$', 'etc.views.tempredirect', {'path': 'request'}),
+	url(r'^service_request.html$', 'etc.views.tempredirect', {'path': 'report_problem'}),
+	# url(r'^service_request.html$', 'etc.views.service'),
     url(r'^(?P<path>.*)$', 'etc.views.page'),
 )
--- etc/views.py
+++ etc/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, redirect
 from django.template import RequestContext
 from etc.user_forms import forms as user_forms
 
@@ -10,3 +10,6 @@ def service(request):
 	form = user_forms.ServiceForm()
 	context = {'form':form,}
 	return render_to_response("workform.html", context, context_instance=RequestContext(request))
+
+def tempredirect(request, path):
+	return redirect("/old/%s.htm" % path)
